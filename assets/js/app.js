3f41ad127be966e3695752a7279b6d5599900d68
susarker1982 <susarker1982@gmail.com>
2020-06-06 19:22:45 +0600

    layout banner

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
-    // assets/js/app.js
-    import Vue from 'vue';
-    import Frontend from './components/Frontend';
-    /**
+// assets/js/app.js
+import Vue from 'vue';
+import Frontend from './components/Frontend';
+
+/**
     * Create a fresh Vue Application instance
-    */
-    new Vue({
-      el: '#app',
-      components: {Frontend}
-    });
+*/
+new Vue({
+  el: '#app',
+  components: {Frontend}
+});   
