2773029fa04563013fb76b98111a8063512899ea
susarker1982 <susarker1982@gmail.com>
2020-06-07 16:55:12 +0600

    front end

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
 // assets/js/app.js
+'use strict';
 import Vue from 'vue';
 import Frontend from './components/Frontend';
-
 /**
     * Create a fresh Vue Application instance
 */
 new Vue({
   el: '#app',
-  components: {Frontend}
+  render: h1 => h1(Frontend)
 });   
